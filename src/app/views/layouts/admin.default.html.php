8a230190c2c77fadafedffc0095c9b5a8bb0b76a
Matt Lo <mattlo.developer@gmail.com>
2013-12-24 22:42:52 -0600

    added ext lib and base install of ext router

--- src/app/views/layouts/admin.default.html.php
+++ src/app/views/layouts/admin.default.html.php
@@ -3,10 +3,33 @@
 	<head>
 		<title>Admin</title>
 		<meta charset="utf-8" />
+		
+		<link href="ext/resources/ext-theme-gray/ext-theme-gray-all.css" type="text/css" rel="stylesheet" />
 	</head>
 	<body>
+		
 		<?php echo $this->content(); ?>
 		
 		<script type="text/javascript" charset="utf-8" src="//cdn.sencha.io/ext-4.2.0-gpl/ext-all.js"></script>
+		
+		<?php
+		$sources = array (
+			'store/Users.js',
+			'controller/Home.js',
+			'controller/Settings.js',
+			'controller/Users.js',
+			'view/home/Index.js',
+			'view/settings/Index.js',
+			'view/users/Edit.js',
+			'view/users/List.js',
+			'view/Viewport.js',
+			'app.js'
+		);
+		?>
+		
+		<script type="text/javascript" src="js/libraries/Router.js"></script>
+		<?php foreach ($sources as $source) { ?>
+			<script type="text/javascript" src="js/admin/<?php echo $source; ?>"></script>
+		<?php } ?>
 	</body>
 </html>
---
+++ src/app/webroot/js/admin/controller/Settings.js
@@ -0,0 +1,6 @@
+Ext.define('AccAdmin.controller.Settings', {
+    extend: 'Ext.app.Controller',
+
+    index: function() {
+    }
+});
